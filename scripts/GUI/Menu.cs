6e51a72317109be388b4d5de0c943a802fa76812
Sander <sandermoolin@gmail.com>
2014-05-25 16:19:02 -0400

    Some progress in development on file reader

---
+++ scripts/GUI/Menu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Menu : MonoBehaviour {
+
+	public static string source;
+
+	AllDefinitions GLOSSARY;
+
+	private void Start ()
+	{
+
+		// load the source file
+		//TextAsset t = Resources.Load("definitions.txt") as TextAsset;
+		StreamReader t = new StreamReader ("Assets/definitions.txt");
+		source = t.ReadToEnd();
+		print ("this:" + source);
+		Current = Position.Welcome;
+		AllDefinitions GLOSSARY = new AllDefinitions();
+	}
+
+	// constructs the glossary at start
+
+
+	// menu state
+	public enum Position
+	{
+		Welcome
+	}
+
+	public Position Current { get; private set; }
+
+	// GUI FUNCTIONS
+	// ==================
+	private void WelcomeScreen ()
+	{
+
+	}
+
+
+
+	// ON GUI()
+	// ==================
+	private void OnGUI ()
+	{
+
+	}
+
+
+	
+
+}
--- scripts/definitions/AllDefinitions.cs
+++ scripts/definitions/AllDefinitions.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.IO;
 
 public class AllDefinitions {
 
@@ -11,17 +12,22 @@ public class AllDefinitions {
 		// A single definition in encased within curly braces {}
 		// All a definition constructor needs is the text from within those curly braces to create itself
 
-		// first, locate the 
 
-		//string[] toSave = Regex.Split ();
-		CreateDefinitions();
+		// split at curly braces
+		List<string> toSave = new List<string>();
+		MatchCollection m = Regex.Matches(Menu.source, @"(.*)");
+		foreach (Capture c in m)
+		{
+			CreateDefinition(c.Value);
+		}
 	}
 
 	// master glossary
 	public static List<Definition> ALL_DEFINITIONS = new List<Definition>();
 
-	private void CreateDefinitions()
+	private void CreateDefinition(string src)
 	{
-		Definition test = new Definition("<word>YEP</word>asdasdasdad<tldr>TL;DR</tldr>");
+		Definition test = new Definition(src);
+		ALL_DEFINITIONS.Add (test);
 	}
 }
